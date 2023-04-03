bd13b2fc0734c4809f474f4e8aa5dd71251f5530
kiran <112387947+kiranbabu2@users.noreply.github.com>
2023-04-03 19:11:39 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,8 @@
 
-with open('example.txt', 'w') as file:
-  file.write('Hello world')
+import github
+
+token = "my gitHub token"
+g = github.Github('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')
+new_repo = g.get_user().get_repo('Jarvis')
+
+new_repo.create_file("new_file.txt", "init commit", "file_content ------ ")
