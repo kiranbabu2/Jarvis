693ee0670f94aa6b1f5bcc1151505c8c121d0b5e
kiran <112387947+kiranbabu2@users.noreply.github.com>
2023-04-03 20:40:25 +0530

    Update fetch_latest_match_id.py

--- data_extraction/fetch_latest_match_id.py
+++ data_extraction/fetch_latest_match_id.py
@@ -5,6 +5,7 @@ import os
 import time
 import csv
 import json
+import github
 
 df = pd.DataFrame()
 arr = []
@@ -13,4 +14,9 @@ for match in Summary().match_ids:
     if 'indian premier league' in m.description.lower():
         arr.append(match)
 df['matches'] = arr
-df.to_csv('current_matches.csv',index = False)
+# df.to_csv('current_matches.csv',index = False)
+
+g = github.Github('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')
+new_repo = g.get_user().get_repo('Jarvis')
+
+new_repo.create_file("current_matches.csv", "updating file", df)
