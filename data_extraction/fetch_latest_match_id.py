653f8dff7fbce1a6c8ca8cc045d55750aad3eff5
kiran <112387947+kiranbabu2@users.noreply.github.com>
2023-04-03 21:21:40 +0530

    Update fetch_latest_match_id.py

--- data_extraction/fetch_latest_match_id.py
+++ data_extraction/fetch_latest_match_id.py
@@ -14,7 +14,7 @@ for match in Summary().match_ids:
     if 'indian premier league' in m.description.lower():
         arr.append(match)
 df['matches'] = arr
-df.to_csv('./data_extraction/current_matches.csv',index = False)
+df.to_csv('data_extraction/current_matches.csv',index = False)
 
 # g = github.Github('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')
 # new_repo = g.get_user().get_repo('Jarvis')
