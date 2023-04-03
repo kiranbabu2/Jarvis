f96ecf3ab2b9c34633bf8f4f837b22652b075498
kiran <112387947+kiranbabu2@users.noreply.github.com>
2023-04-03 19:13:54 +0530

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,8 +1,7 @@
-
 import github
 
 token = "my gitHub token"
 g = github.Github('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')
 new_repo = g.get_user().get_repo('Jarvis')
 
-new_repo.create_file("new_file.txt", "init commit", "file_content ------ ")
+new_repo.create_file("new_file1.txt", "init commit", "file_content ------ ")
