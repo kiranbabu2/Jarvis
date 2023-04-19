c68e9187d63b9de5493bfb02f9b5e04fe758ccb9
ktikku <tikku_karan@lilly.com>
2023-04-19 19:39:05 +0530

    added app code

---
+++ app/src/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <>
+         <img src="/assets/images/headerImage.png" alt="header" width="100%"/>
+        </>
+    )
+}
+
+export default Header;
